1e8b7c25e6ac1608784d5e1430fd4c5f444b9f7a
Den Isahac <denisahac@denisahac.local>
2021-12-02 00:25:57 +0800

    Product grid and details page

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
+import useStyles from './app.style';
+
 function Media( props ) {
+    const classes = useStyles();
     const [ url, setUrl ] = useState( null );
     
     if( ! props.media ) return null;
@@ -20,34 +23,57 @@ function Media( props ) {
     }, [] );
 
     return (
-        <img src={url} />
+        <img className={ classes.product__image } src={url} />
     );
 }
 
+
 function App() {
+    const classes = useStyles();
+
     const [ products, setProducts ]  = useState( [] );
+    const [ product, setProduct ] = useState( null );
 
     useEffect( () => {
         $.ajax( {
             method: 'GET',
-            url: 'https://jteststage.wpengine.com/wp-json/wp/v2/products',
+            url: 'https://jteststage.wpengine.com/wp-json/wp/v2/products?per_page=100',
         } )
         .done( data => {
             setProducts( data );
+            console.log( data );
         } );
     }, [] );
+
+
     
     return (
        <>
-            <div className="products">
+            <div className={ classes.row }>
             { products.map( ( product ) => 
-                <div className="product" key={ product.id }>
-                    <Media media={product.featured_media} />
-                    <h2>{ product.title.rendered }</h2>
-                    <div dangerouslySetInnerHTML={ { __html: product.content.rendered } } />
+                <div className={ classes.column } key={ product.id }>
+                    <div className={ classes.product } onClick={ () => setProduct( product ) }>
+                        <Media media={product.featured_media}/>
+                        <h2>{ product.title.rendered }</h2>
+                        <div dangerouslySetInnerHTML={ { __html: product.excerpt.rendered } } />
+                    </div>
                 </div>
             ) }
             </div>
+        
+            { product &&
+            <div className={ classes.product__details }>
+                <a className={ classes.product__details__close } onClick={ () => setProduct( null ) }>&times;</a>
+                <div className={ classes.clearfix }></div>
+
+                <div className={ classes.product__details__inner }>
+
+                    <Media media={product.featured_media}/>
+                    <h2>{ product.title.rendered }</h2>
+                    <div dangerouslySetInnerHTML={ { __html: product.content.rendered } } />
+                </div>
+            </div>
+            }
         </>
     );
 }
---
+++ src/app.style.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles( {
+    clearfix: {
+        display: 'block',
+        content: '.',
+        clear: 'both',
+        color: 'transparent'
+    },
+
+    row: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    column: {
+        flex: '0 0 25%'
+    },
+    product: {
+        padding: 20,
+        marginBottom: '1rem',
+        cursor: 'pointer'
+    },
+
+    'product__image': {
+        maxWidth: '100%',
+        height: 200,
+    },
+
+    'product__details': {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+
+    'product__details__close': {
+        'float': 'right',
+        fontSize: 30,
+        cursor: 'pointer'
+    },
+
+    'product__details__inner': {
+        padding: 20
+    }
+} );
+
+export default useStyles;
+
